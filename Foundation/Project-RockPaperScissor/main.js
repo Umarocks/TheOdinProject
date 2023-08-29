3336312e5f964eef0a125178b967aed25805506b
Umar Aziz <umarocks@hotmail.com>
2023-08-28 19:24:55 -0500

    did some JS. trying to figure out how to search array's index by taking string as an input

--- Foundation/Project-RockPaperScissor/main.js
+++ Foundation/Project-RockPaperScissor/main.js
@@ -1,6 +1,24 @@
-function getComputerChoice(max){
+function getComputerChoice(){
+    const max = 3;
     const computerChoice = Math.floor(Math.random() * max);
-    console.log(computerChoice)
+    console.log("Computer Selected " + stringArray[computerChoice]);
+    return(stringArray[computerChoice]);
 }
 
-getComputerChoice(3);
+function getPlayerChoice(){
+    const playerSelection = prompt("Please select any number from 1:rock 2:paper 3:scissor");
+    if (playerSelection > 3) {
+        throw new Error('Value should not be greater than 3');
+      }
+    console.log("Player Selected " + stringArray[playerSelection%3])
+    return(playerSelection)
+}
+
+function Result(computerChoice,playerSelection){
+    
+}
+
+
+const stringArray = ["rock","paper","scissor"]
+Result(getComputerChoice(),getPlayerChoice())
+
