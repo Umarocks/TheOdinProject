da6d33c01f9da301bf1493ae0d8d11e81a69ab65
Umarocks <umarocks@hotmail.com>
2023-09-03 23:51:34 -0500

    .

--- Foundation/Javascript Excersise/javascript-exercises-main/07_tempConversion/tempConversion.js
+++ Foundation/Javascript Excersise/javascript-exercises-main/07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let tempInCelsius = (temp-32)/1.8
+  tempInCelsius=Math.round(tempInCelsius*10)/10
+  return tempInCelsius
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let tempInFahrenheit = (temp*1.8)+32;
+  tempInFahrenheit = Math.round(tempInFahrenheit*10)/10;
+  console.log(tempInFahrenheit);
+  return tempInFahrenheit;
 };
 
 // Do not edit below this line
--- Foundation/Javascript Excersise/javascript-exercises-main/08_calculator/calculator.js
+++ Foundation/Javascript Excersise/javascript-exercises-main/08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(num1 ,num2) {
+	return (num1+num2);
 };
 
 const subtract = function() {
-	
+	return (num1-num2);
 };
 
-const sum = function() {
-	
+const sum = function(...args) {
+  let sum=0;
+  for(i=0;i<args.length;i++){
+    sum+=args[i];
+  }
+	return (sum);
 };
 
-const multiply = function() {
-
+const multiply = function(num1 ,num2) {
+  return (num1*num2);
 };
 
 const power = function() {
-	
+	return (num1**num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num1) {
+  let factorial=1;
+	for(i=num1;i>=1;i--){
+    factorial*=i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
