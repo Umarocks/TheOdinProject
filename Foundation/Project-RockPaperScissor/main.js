f7ffe95f01dd6db0edccfd6295719fecf44d927a
Umarocks <umarocks@hotmail.com>
2023-09-09 18:33:33 -0500

    started with css

--- Foundation/Project-RockPaperScissor/main.js
+++ Foundation/Project-RockPaperScissor/main.js
@@ -6,11 +6,11 @@ function getComputerChoice(){
 }
 
 function getPlayerChoice(){
-    let playerSelection = prompt("Please select any number from 1:rock 2:paper 3:scissor");
+    // let playerSelection = prompt("Please select any number from 1:rock 2:paper 3:scissor");
     playerSelection=playerSelection.toLowerCase();
-    // console.log(playerSelection)
+    
     const playerSelectionIndex = stringArray.indexOf(playerSelection);
-    // console.log(playerSelectionIndex);
+    
     console.log("Player Selected " + stringArray[playerSelectionIndex])
     return(playerSelectionIndex)
 }
@@ -23,17 +23,17 @@ function Result(computerChoice,playerSelectionIndex){
                 // playerScore++;
             }
             else if(playerSelectionIndex==1){ //paper
-                // console.log("You Won! YAY");
+                
                 playerScore++;
             }
             else{ //scissor
-                // console.log("Computer Won :(");
+                
                 computerScore++;
             }
         }
         else if(computerChoice==1){ //paper
             if(playerSelectionIndex==0){ //rock
-                // console.log("Computer Won :(");
+                
                 computerScore++;
             }
             else if(playerSelectionIndex==1){ //paper
@@ -42,17 +42,17 @@ function Result(computerChoice,playerSelectionIndex){
                 // playerScore++;
             }
             else{ //scissor
-                // console.log("You Won! YAY");
+                
                 playerScore++;
             }
         }
         else if(computerChoice==2){ //scissor
             if(playerSelectionIndex==0){ //rock
-                // console.log("You Won! YAY");
+                
                 playerScore++;
             }
             else if(playerSelectionIndex==1){ //paper
-                // console.log("Computer Won :(");
+                
                 computerScore++;
             }
             else{ //scissor
