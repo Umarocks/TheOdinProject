77b1e14b739e5db900506a145362138c0be54938
Umarocks <umarocks@hotmail.com>
2023-09-13 10:51:09 -0500

    got the dom working well for inputs and click are working fine. starting work on score and visuals now

--- Foundation/Project-RockPaperScissor/main.js
+++ Foundation/Project-RockPaperScissor/main.js
@@ -1,114 +1,125 @@
-function getComputerChoice() {
-  const max = 3;
-  const computerChoice = Math.floor(Math.random() * max);
-  console.log("Computer Selected " + stringArray[computerChoice]);
-  return computerChoice;
-}
+document.addEventListener("DOMContentLoaded", function () {
 
-function getPlayerChoice() {
-  // let playerSelection = prompt("Please select any number from 1:rock 2:paper 3:scissor");
-  playerSelection = playerSelection.toLowerCase();
-
-  const playerSelectionIndex = stringArray.indexOf(playerSelection);
-
-  console.log("Player Selected " + stringArray[playerSelectionIndex]);
-  return playerSelectionIndex;
-}
-
-function Result(computerChoice, playerSelectionIndex) {
-  if (computerChoice == 0) {
-    //rock
-    if (playerSelectionIndex == 0) {
+  const stringArray = ["rock", "paper", "scissor"];
+  let computerScore = 0;
+  let playerScore = 0;
+  
+  function getComputerChoice() {
+    const max = 3;
+    const computerChoice = Math.floor(Math.random() * max);
+    console.log("Computer Selected " + stringArray[computerChoice]);
+    return computerChoice;
+  }
+  
+  //  function getPlayerChoice() {
+  //   // let playerSelection = prompt("Please select any number from 1:rock 2:paper 3:scissor");
+  //   let playerSelection;
+  //   document.addEventListener("click", function () {
+  //     var rockClick = document.querySelector(".playerSide .rock");
+  //     var paperClick = document.querySelector(".playerSide .paper");
+  //     var scissorClick = document.querySelector(".playerSide .scissor");
+  //     rockClick.addEventListener("click", function () {
+  //       playerSelection = "rock";
+  //     });
+  //     paperClick.addEventListener("click", function () {
+  //       playerSelection = "paper";
+  //     });
+  //     scissorClick.addEventListener("click", function () {
+  //       playerSelection = "scissor";
+  //     });
+      
+  //     const playerSelectionIndex = stringArray.indexOf(playerSelection);
+  //     console.log("Player Selected " + stringArray[playerSelectionIndex]);
+  //     return playerSelectionIndex;
+  //   });
+  // }
+  function getPlayerChoice() {
+    return new Promise((resolve) => {
+      var rockClick = document.querySelector(".playerSide .rock");
+      var paperClick = document.querySelector(".playerSide .paper");
+      var scissorClick = document.querySelector(".playerSide .scissor");
+  
+      rockClick.addEventListener("click", () => {
+        resolve("rock");
+      });
+  
+      paperClick.addEventListener("click", () => {
+        resolve("paper");
+      });
+  
+      scissorClick.addEventListener("click", () => {
+        resolve("scissor");
+      });
+    });
+  }
+  
+  function Result(computerChoice, playerSelectionIndex) {
+    if (computerChoice == 0) {
       //rock
-      // console.log("The Game Ended in a Draw");
-      // computerScore++;
-      // playerScore++;
-    } else if (playerSelectionIndex == 1) {
+      if (playerSelectionIndex == 0) {
+        //rock
+        // console.log("The Game Ended in a Draw");
+        // computerScore++;
+        // playerScore++;
+      } else if (playerSelectionIndex == 1) {
+        //paper
+  
+        playerScore++;
+      } else {
+        //scissor
+  
+        computerScore++;
+      }
+    } else if (computerChoice == 1) {
       //paper
-
-      playerScore++;
-    } else {
+      if (playerSelectionIndex == 0) {
+        //rock
+  
+        computerScore++;
+      } else if (playerSelectionIndex == 1) {
+        //paper
+        // console.log("The Game Ended in a Draw");
+        // computerScore++;
+        // playerScore++;
+      } else {
+        //scissor
+  
+        playerScore++;
+      }
+    } else if (computerChoice == 2) {
       //scissor
-
-      computerScore++;
-    }
-  } else if (computerChoice == 1) {
-    //paper
-    if (playerSelectionIndex == 0) {
-      //rock
-
-      computerScore++;
-    } else if (playerSelectionIndex == 1) {
-      //paper
-      // console.log("The Game Ended in a Draw");
-      // computerScore++;
-      // playerScore++;
-    } else {
-      //scissor
-
-      playerScore++;
-    }
-  } else if (computerChoice == 2) {
-    //scissor
-    if (playerSelectionIndex == 0) {
-      //rock
-
-      playerScore++;
-    } else if (playerSelectionIndex == 1) {
-      //paper
-
-      computerScore++;
-    } else {
-      //scissor
-      // console.log("The Game Ended in a Draw");
-      // computerScore++;
-      // playerScore++;
+      if (playerSelectionIndex == 0) {
+        //rock
+  
+        playerScore++;
+      } else if (playerSelectionIndex == 1) {
+        //paper
+  
+        computerScore++;
+      }
     }
   }
-}
-
-function playRound() {
-  Result(getComputerChoice(), getPlayerChoice());
-  console.log("Score:computer-" + computerScore + " player-" + playerScore);
-}
-
-function game() {
-  for (i = 0; i < 5; i++) {
-    playRound();
+  
+  async function playRound() {
+    let playerChoice = await getPlayerChoice();
+    const playerSelectionIndex = stringArray.indexOf(playerChoice);
+    let computerChoice = getComputerChoice();
+    Result(computerChoice, playerSelectionIndex);
+    console.log("Score:computer-" + computerScore + " player-" + playerScore);
   }
-  if (computerScore > playerScore) {
-    console.log("Computer Won! Better Luck Next Time");
-  } else {
-    console.log("You Won! YAY!");
+  
+  async function game() {
+    for (i = 0; i < 5; i++) {
+      await playRound();
+    }
+    if (computerScore > playerScore) {
+      console.log("Computer Won! Better Luck Next Time");
+    } else {
+      console.log("You Won! YAY!");
+    }
+    console.log("Game End");
   }
-  console.log("Game End");
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-  var rockClick = document.querySelector(".playerSide .rock");
-
-  rockClick.addEventListener("click", function () {
-    alert("rock clicked");
-  });
+  
+  game();
+  
 });
-
-document.addEventListener("DOMContentLoaded", function () {
-  var paperClick = document.querySelector(".playerSide .paper");
-
-  paperClick.addEventListener("click", function () {
-    alert("paper clicked");
-  });
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-  var scissorClick = document.querySelector(".playerSide .scissor");
-
-  scissorClick.addEventListener("click", function () {
-    alert("scissor clicked");
-  });
-});
-
-const stringArray = ["rock", "paper", "scissor"];
-let computerScore = 0;
-let playerScore = 0;
-game();
