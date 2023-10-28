07923a2b5b1a828f72d0f0f132b3e29eb1b2cf5d
Umarocks <umarocks@hotmail.com>
2023-10-28 15:36:20 -0500

    the adding of new books is working now

--- Javascript/Project-Library/script.js
+++ Javascript/Project-Library/script.js
@@ -1,46 +1,51 @@
-const library = []
+const library = [];
 
-const bookInput = document.getElementById('bookInput');
-const authorInput = document.getElementById('authorInput');
-const statuss = document.getElementById('statusInput');
-const submit = document.getElementById('submit');
+const bookInput = document.getElementById("bookInput");
+const authorInput = document.getElementById("authorInput");
+const statuss = document.getElementById("statusInput");
+const submit = document.getElementById("submit");
 
-submit.addEventListener('click',function(){
-    addBook();
-    console.log(library)
-})
+submit.addEventListener("click", function () {
+  addBook();
+  console.log(library);
+});
 
-function addBook(){
-    const newBook = {
-        book:bookInput.value,
-        author:authorInput.value,
-        status:statuss.value
-    }
-    library.push(newBook);
+function addBook() {
+  const newBook = {
+    book: bookInput.value,
+    author: authorInput.value,
+    status: statuss.value,
+  };
+  library.push(newBook);
+  render();
 }
 
-function render(){
-    library.forEach(element)
+function render() {
+    const insertionDiv = document.getElementById("entryContainer");
+    insertionDiv.innerHTML =' ';
+  library.forEach(function (book, index) {
+    console.log("Book " + (index + 1) + ":");
+    console.log("Book Name: " + book.book);
+    console.log("Author: " + book.author);
+    console.log("-------------------");
+    var entryHTML = `
+        <div class="entry">
+          <div class="bookName">
+            <h4>${book.book}</h4>
+          </div>
+          <div class="authorName">
+            <h4>${book.author}</h4>
+          </div>
+          <div class="read">
+            <button>${book.status}</button>
+          </div>
+          <div class="delete">
+            <button>Delete</button>
+          </div>
+        </div>
+      `;
+    
+    
+    insertionDiv.innerHTML += entryHTML;
+  });
 }
-
-
-
-
-// var entryHTML = `
-//     <div class="entry">
-//       <div class="bookName">
-//         <h4>${book.book}</h4>
-//       </div>
-//       <div class="authorName">
-//         <h4>${book.author}</h4>
-//       </div>
-//       <div class="read">
-//         <button>${book.status}</button>
-//       </div>
-//       <div class="delete">
-//         <button>Delete</button>
-//       </div>
-//     </div>
-//   `;
-//     const insertionDiv = document.getElementsByClassName('entryContainer');
-//     insertionDiv.innerHTML = entryHTML;
