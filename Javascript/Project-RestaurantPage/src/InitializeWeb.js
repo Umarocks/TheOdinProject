2df3a940dec86b2dedf11fee8f2628401f6c7eb0
Umarocks <umarocks@hotmail.com>
2023-11-06 18:34:24 -0600

    done with the rendering of the home page

--- Javascript/Project-RestaurantPage/src/InitializeWeb.js
+++ Javascript/Project-RestaurantPage/src/InitializeWeb.js
@@ -1,47 +1,39 @@
+import loadHome from "./home";
 
-
-function createHeader(){
-    const header = document.createElement('header');
-    header.classList.add("header");
-    const restaurantName = document.createElement("h1");
-    restaurantName.textContent="Pizza Factory";
-    restaurantName.classList.add("restaurantName");
-    header.appendChild(restaurantName);
-    header.appendChild(createNavbar());
-    return header;
+function createHeader() {
+  const header = document.createElement("header");
+  header.classList.add("header");
+  const restaurantName = document.createElement("h1");
+  restaurantName.textContent = "Pizza Factory";
+  restaurantName.classList.add("restaurantName");
+  header.appendChild(restaurantName);
+  header.appendChild(createNavbar());
+  return header;
 }
 
-function createNavbar(){
-    const navBar = document.createElement("nav");
+function createNavbar() {
+  const navBar = document.createElement("nav");
 
-    const homeButton = document.createElement("button");
-    homeButton.classList.add("homeButton");
-    homeButton.textContent="HOME";
+  const homeButton = document.createElement("button");
+  homeButton.classList.add("homeButton");
+  homeButton.textContent = "HOME";
 
+  const menuButton = document.createElement("button");
+  menuButton.classList.add("menuButton");
+  menuButton.textContent = "Menu";
 
-    const menuButton = document.createElement("button");
-    menuButton.classList.add("menuButton");
-    menuButton.textContent="Menu";
+  const contactButton = document.createElement("button");
+  contactButton.classList.add("contactButton");
+  contactButton.textContent = "CONTACT";
+  navBar.appendChild(homeButton);
+  navBar.appendChild(menuButton);
+  navBar.appendChild(contactButton);
 
-
-    const contactButton = document.createElement("button");
-    contactButton.classList.add("contactButton");
-    contactButton.textContent="CONTACT";
-    navBar.appendChild(homeButton);
-    navBar.appendChild(menuButton);
-    navBar.appendChild(contactButton);
-    
-    return navBar;
-
+  return navBar;
 }
 function initializeWebsite() {
-    const content = document.getElementById("content");
-  
-    content.appendChild(createHeader());
-    // content.appendChild(createMain());
-    // content.appendChild(createFooter());
-  
-    // setActiveButton(document.querySelector(".button-nav"));
-    // loadHome();
-  }
+  const content = document.getElementById("content");
+  content.appendChild(createHeader());
+  loadHome();
+}
 export default initializeWebsite;
---
+++ Javascript/Project-RestaurantPage/src/home.js
@@ -0,0 +1,25 @@
+function createHome() {
+  const home = document.createElement("div");
+  home.classList.add("homeContainer");
+  const homeImage = document.createElement("img");
+  homeImage.src="images/chef.png";
+  home.appendChild(Paragraph("Best pizza in your country"));
+  home.appendChild(Paragraph("Made with passion since 1908"));
+  home.appendChild(homeImage);
+  home.appendChild(Paragraph("Order online or visit us!"));
+  return home;
+}
+
+
+function Paragraph(string){
+    const paragraphText = document.createElement("p");
+    paragraphText.textContent=string;
+    return paragraphText;
+}
+
+function loadHome() {
+    const content = document.querySelector(".content");
+    content.appendChild(createHome());
+}
+
+export default loadHome;
