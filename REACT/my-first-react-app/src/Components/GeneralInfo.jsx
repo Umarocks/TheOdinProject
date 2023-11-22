a9ec5080f10bc5d38d580a76eebff5f772f18149
Umarocks <umarocks@hotmail.com>
2023-11-21 20:07:15 -0600

    finally understood how to use useState

--- REACT/my-first-react-app/src/Components/GeneralInfo.jsx
+++ REACT/my-first-react-app/src/Components/GeneralInfo.jsx
@@ -2,22 +2,28 @@ import React from "react";
 import GeneralInfoForm from "./GeneralInfoForm";
 import { useState } from "react";
 export default function GeneralInfo(props) {
-  const[generalInfoFormShow , setGeneralInfoForm] = useState(false);
+  const [generalInfoFormShow, setGeneralInfoForm] = useState(false);
 
-  const handleGeneralInfoForm= () => {
+  const handleGeneralInfoForm = () => {
     setGeneralInfoForm(!generalInfoFormShow);
-  }
+  };
   return (
-
-
     <div className="generalInfoContainer">
       <div className="generalInfo">
-        <img src="" alt="" />
-        <h3>General Information</h3>
-        <img src="https://rmathr.github.io/cv-project/9a85e2732c3a2bab601e.png" alt="" onClick={handleGeneralInfoForm} />
+        <div className="GenerealInfoIconAndHeading">
+          <img src="" alt="" />
+          <h3>General Information</h3>
+        </div>
+        <div className="GeneralInfoDropDownIcon">
+          <img
+            src="https://rmathr.github.io/cv-project/9a85e2732c3a2bab601e.png"
+            alt=""
+            onClick={handleGeneralInfoForm}
+          />
+        </div>
       </div>
       <div className="GeneralInfoDropDown">
-        {generalInfoFormShow && <GeneralInfoForm/>}
+        {generalInfoFormShow && <GeneralInfoForm />}
       </div>
     </div>
   );
--- REACT/my-first-react-app/src/Components/GeneralInfoForm.jsx
+++ REACT/my-first-react-app/src/Components/GeneralInfoForm.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React from "react";
 
-export default function GeneralInfoForm(props) {
+export default function GeneralInfoForm() {
   return (
-    <div>GeneralInforForm</div>
-    //make form with input and take some param 
-  )
+    <>
+      <div className="Name">
+        <h3>Full Name</h3>
+        <input type="text" />
+      </div>
+      <div className="Email">
+        <h3>Email</h3>
+        <input type="email" />
+      </div>
+      <div className="Phone Number">
+        <h3>Full Name</h3>
+        <input type="number" />
+      </div>
+      <div className="City,Province">
+        <h3>Full Name</h3>
+        <input type="text" />
+      </div>
+      <div className="SaveButton">
+        <button>
+          <img src="" alt="" />
+          Save
+        </button>
+      </div>
+    </>
+  );
 }
