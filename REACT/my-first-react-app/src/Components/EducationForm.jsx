7e31e953937d9f6406be488c2d84c220e314d24b
Umarocks <umarocks@hotmail.com>
2023-11-25 18:53:09 -0600

    .

--- REACT/my-first-react-app/src/Components/EducationForm.jsx
+++ REACT/my-first-react-app/src/Components/EducationForm.jsx
@@ -1,5 +1,41 @@
 import React from "react";
 
 export default function EducationForm() {
-  return <div>EducationForm</div>;
+  return (
+    <>
+      <div className="Degree">
+        <h3>Degree</h3>
+        <input type="text" />
+      </div>
+      <div className="School">
+        <h3>School</h3>
+        <input type="text" />
+      </div>
+      <div className="City">
+        <h3>City</h3>
+        <input type="text" />
+      </div>
+      <div className="Country">
+        <h3>Country</h3>
+        <input type="text" />
+      </div>
+      <div className="Date">
+        <div className="StartDate">
+          <h3>Start Date</h3>
+          <input type="date" name="StartDateInput" id="" />
+        </div>
+        <div className="EndDate">
+          <h3>End Date</h3>
+          <input type="date" name="EndDateInput" id="" />
+        </div>
+      </div>
+      <div className="SaveButton">
+        <button>
+          <path d="M0 0h24v24H0z" fill="none"></path>
+          <path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z"></path>
+          Save
+        </button>
+      </div>
+    </>
+  );
 }
