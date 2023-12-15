279dd499a7b29198a2bfcf244122022385fbc8de
Umarocks <umarocks@hotmail.com>
2023-12-14 21:47:08 -0600

    started with CV display and would finish the project by sunday max, already running late right now

--- REACT/my-first-react-app/src/App.jsx
+++ REACT/my-first-react-app/src/App.jsx
@@ -5,30 +5,35 @@ import EducationInfo from "./Components/EducationInfo";
 import ProfessionalExperience from "./Components/ProfessionalExperience";
 import CVGenerator from "./Components/CVGenerator";
 import SaveResume from "./Components/SaveResume";
-
+import Display from "./Components/Display";
 function App() {
   const [generalInfoForm, setGeneralInfoForm] = useState("");
   const [educationInfoForm, setEducationInfoForm] = useState([]);
   const [professionalInfoForm, setProfessionalInfoForm] = useState([]);
+  const [generalEmpty, setGeneralEmpty] = useState(true);
+  const [educationEmpty, setEducationEmpty] = useState(true);
+  const [professionalEmpty, setProfessionalEmpty] = useState(true);
 
   function saveInput(obj) {
     switch (obj.form) {
       case "GeneralInfo":
         setGeneralInfoForm(obj);
         console.log(generalInfoForm);
+        setGeneralEmpty(false);
         break;
       case "EducationInfo":
-        const x = [...educationInfoForm,obj];
+        const x = [...educationInfoForm, obj];
         setEducationInfoForm(x);
-        console.log(educationInfoForm)
+        console.log(educationInfoForm);
+        setEducationEmpty(false);
         break;
-        case "ProfessionalInfo":
-        const y = [...professionalInfoForm,obj];
+      case "ProfessionalInfo":
+        const y = [...professionalInfoForm, obj];
         setProfessionalInfoForm(y);
-        console.log(professionalInfoForm)
+        console.log(professionalInfoForm);
+        setProfessionalEmpty(false);
         break;
-      }
-    
+    }
   }
 
   return (
@@ -46,7 +51,15 @@ function App() {
             <EducationInfo saveInput={saveInput} />
             <ProfessionalExperience saveInput={saveInput} />
           </div>
-          <div className="Display"></div>
+          <div className="Display">
+            {(!generalEmpty || !educationEmpty || !professionalEmpty) && (
+              <Display
+                generalInfoForm={generalInfoForm}
+                professionalInfoForm={professionalInfoForm}
+                educationInfoForm={educationInfoForm}
+              />
+            )}
+          </div>
         </div>
       </div>
     </>
---
+++ REACT/my-first-react-app/src/Components/Display.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+// const generalInfoObj = {
+//   id: generalInfoObjid,
+//   name: nameRef.current.value,
+//   email: emailRef.current.value,
+//   phone: phoneRef.current.value,
+//   city: cityRef.current.value,
+//   form: "GeneralInfo",
+// };
+// const educationFormObj = {
+//   id: id,
+//   degree: degreeRef.current.value,
+//   school: schoolRef.current.value,
+//   city: cityRef.current.value,
+//   country: countryRef.current.value,
+//   startDate: startDateRef.current.value,
+//   endDate: endDateRef.current.value,
+//   form: "EducationInfo",
+// };
+function Display(props) {
+  return (
+    <>
+      <div className="displayContainer">
+        <div className="generalInfoDisplay">
+          <div
+            className="generalName"
+            style={{ borderBottom: "2px black solid" }}
+          >
+            <h1>{props.generalInfoForm.name}</h1>
+          </div>
+          <div className="generalemailetc">
+            <p>
+              {props.generalInfoForm.email +
+                " " +
+                props.generalInfoForm.phone +
+                " " +
+                props.generalInfoForm.city}
+            </p>
+          </div>
+        </div>
+        <div className="educationalInfoDisplay"></div>
+        <div className="professionalInfoDisplay"></div>
+      </div>
+    </>
+  );
+}
+
+export default Display;
